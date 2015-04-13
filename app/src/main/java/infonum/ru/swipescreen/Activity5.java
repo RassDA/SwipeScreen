990292edebfd798382b48ffdf18125d21cec12d8
RassDA <infonumru@gmail.com>
2015-04-13 22:24:40 +0300

    запускается. Требовалась установка флага

---
+++ app/src/main/java/infonum/ru/swipescreen/Activity5.java
@@ -0,0 +1,18 @@
+package infonum.ru.swipescreen;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Activity5 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout5);
+    }
+
+
+}
